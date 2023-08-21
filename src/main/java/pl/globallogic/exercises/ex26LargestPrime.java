d7902f61c356dda0b8eff0d0eaf5ec6082c52383
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-08-21 21:34:51 +0200

    exercises

---
+++ src/main/java/pl/globallogic/exercises/ex26LargestPrime.java
@@ -0,0 +1,25 @@
+package pl.globallogic.exercises;
+
+public class ex26LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+        int largestPrime = 2; 
+        while (number > 1) {
+            if (number % largestPrime == 0) {
+                number /= largestPrime;
+            } else {
+                largestPrime++;
+            }
+        }
+        return largestPrime;
+    }
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+}
